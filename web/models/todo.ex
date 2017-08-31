855623add2251ccb6a87b88d214e1c290f9075ef
Matt Kerchner <manafount@gmail.com>
2017-08-30 22:31:28 -0700

    added required completion parameter for todos

--- web/models/todo.ex
+++ web/models/todo.ex
@@ -8,12 +8,13 @@ defmodule PhoenixTodos.Todo do
     timestamps()
   end
 
+  @required_fields ~w(description complete)
+  
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:description])
-    |> validate_required([:description])
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
   end
 end
